58298d1ce1a42548dde8f49e9002b655afb92d5d
Abbas Shaikh <61411096+Abbas12321@users.noreply.github.com>
2021-12-24 19:51:42 +0530

    Create Stdin and Stdout.java

---
+++ Java/Basic of Java/Stdin and Stdout.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class driver{
+public static void main(String args[]){
+Scanner scanner = new Scanner(System.in);
+int n1 = scanner.nextInt();
+int n2 = scanner.nextInt();
+scanner.close();
+System.out.println(n1);
+System.out.println(n2);
+}
+}
