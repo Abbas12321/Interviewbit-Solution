9d6bd50ba6cb034f6cbb2f4effdaccb073945d7b
Abbas Shaikh <61411096+Abbas12321@users.noreply.github.com>
2021-12-24 08:44:22 +0530

    Create Evaluate Expression.py

---
+++ Stacks and Queues/Evaluate Expression.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param A : list of strings
+    # @return an integer
+    def evalRPN(self, A):
+        toOperate = []
+        operators = ["+", "-", "*", "/"]
+        for e in A:
+            if e not in operators:
+                toOperate.append(int(e))
+            else:
+                opTwo = toOperate.pop()
+                opOne = toOperate.pop()
+                if e == "+":
+                    toOperate.append(opOne + opTwo)
+                elif e == "-":
+                    toOperate.append(opOne - opTwo)
+                elif e == "*":
+                    toOperate.append(opOne * opTwo)
+                elif e == "/":
+                    toOperate.append(opOne // opTwo)
+        return toOperate.pop()
