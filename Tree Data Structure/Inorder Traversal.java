cbc3c2b6f8ac8880e20b026c54143284cc21b5db
Abbas Shaikh <61411096+Abbas12321@users.noreply.github.com>
2022-01-04 19:39:50 +0530

    Create Inorder Traversal.java

---
+++ Tree Data Structure/Inorder Traversal.java
@@ -0,0 +1,20 @@
+public class Solution {
+  public ArrayList<Integer> inorderTraversal(TreeNode A) {
+    ArrayList<Integer> list = new ArrayList<>();
+    Stack<TreeNode> stack = new Stack<>();
+    while (A != null) {
+      stack.push(A);
+      A = A.left;
+    }
+    while (!stack.isEmpty()) {
+      TreeNode removed = stack.pop();
+      list.add(removed.val);
+      TreeNode right = removed.right;
+      while (right != null) {
+        stack.push(right);
+        right = right.left;
+      }
+    }
+    return list;
+  }
+}
