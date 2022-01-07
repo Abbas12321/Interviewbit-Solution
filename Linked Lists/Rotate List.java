8a1e9a3ee40d2cfe81d9578803e5a2ff42805a55
Abbas Shaikh <61411096+Abbas12321@users.noreply.github.com>
2022-01-07 19:15:13 +0530

    Create Rotate List.java

---
+++ Linked Lists/Rotate List.java
@@ -0,0 +1,43 @@
+public class Solution {
+    public ListNode rotateRight(ListNode A, int B) {
+        int l = getLength(A);
+        ListNode curr = A;
+        int stop = B%l;
+        if(stop == 0) return A;
+        stop = l - stop;
+        
+        A = rotate(A, stop);
+        return A;
+    }
+    
+    public ListNode rotate(ListNode A, int stop) {
+        ListNode curr = A;
+        ListNode head = null;
+        while(stop != 1) {
+            curr = curr.next;
+            stop--;
+        }
+        
+        head = curr.next;
+        curr.next = null;
+        
+        ListNode temp = head;
+        while(temp.next != null) {
+            temp = temp.next;
+        }
+        
+        temp.next = A;
+        
+        return head;
+    }
+    
+    public int getLength(ListNode A) {
+        int l = 0;
+        while (A != null) {
+            A = A.next;
+            l++;
+        }
+        
+        return l;
+    }
+}
