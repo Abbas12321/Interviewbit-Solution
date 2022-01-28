23c40eac5effd594589183496820ca7b99d67df1
Abbas Shaikh <61411096+Abbas12321@users.noreply.github.com>
2022-01-28 12:09:39 +0530

    Update Word Break.cpp

--- Dynamic Programming/Word Break.cpp
+++ Dynamic Programming/Word Break.cpp
@@ -1,34 +1,22 @@
-bool search(string A, vector<string> B){
-    for(int i = 0; i < B.size(); i++){
-        if(B[i] == A){
-            return true;
-        }
-    }
-    
-    return false;
-}
-
+unordered_set<string> dict;
+vector<bool> dp;
 int Solution::wordBreak(string A, vector<string> &B) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-
-    int n = A.size();
- 
-    vector<int> temp(n+1, 0);
-    temp[n] = 1;
-    
-    for(int i = n-1; i >= 0; i--){
-        for(int j = i; j < n; j++){
-            string s = A.substr(i, j-i+1);
-            if(search(s, B) && temp[j+1] == 1){
-                temp[i] = 1;
-                break;
+    dict.clear();
+    dp.clear();
+    dp.resize(A.size() + 1, 0);
+    for (auto it : B) dict.insert(it);
+    dp[A.size()] = 1;
+    for (int i = A.size() - 1; i >= 0; i--) {
+        string tmp = "";
+        for (int j = i; j < A.size(); j++) {
+            if (dp[i]) break;
+            tmp += A[j];
+            if (dict.find(tmp) != dict.end()) {
+                dp[i] = dp[j + 1];
             }
         }
     }
-    
-    return temp[0];
-
+    if (dp[0])
+        return 1;
+    return 0;
 }
